91f4996de28fcf89f2467309510779fee1902d53
Nick Kirby <nrkirb@gmail.com>
2017-11-02 19:44:00 +0000

    Complete redesign

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-$(document).foundation()
+document.addEventListener("DOMContentLoaded", function (event) {
+    document.getElementById("show-image").addEventListener("mouseover", function () {
+        document.getElementById("img-circle").style.display = "block"
+    })
+
+    document.getElementById("show-image").addEventListener("mouseout", function () {
+        document.getElementById("img-circle").style.display = "none"
+    })
+});
+
