d3de23f7d1ba6b8ae2d16943f581eb2638d334a6
Nick Kirby <nrkirb@gmail.com>
2017-11-23 20:04:41 +0000

    Work on social icons

--- js/app.js
+++ js/app.js
@@ -6,5 +6,21 @@ document.addEventListener("DOMContentLoaded", function (event) {
     document.getElementById("show-image").addEventListener("mouseout", function () {
         document.getElementById("img-circle").style.display = "none"
     })
+
+    document.getElementById("twitter").addEventListener("mouseover", function () {
+        this.src = 'img/twitter-inverted.svg';
+    })
+
+    document.getElementById("twitter").addEventListener("mouseout", function () {
+        this.src = 'img/twitter1.svg';
+    })
+
+    document.getElementById("linkedin").addEventListener("mouseover", function () {
+        this.src = 'img/linkedin-inverted.svg';
+    })
+
+    document.getElementById("linkedin").addEventListener("mouseout", function () {
+        this.src = 'img/linkedin.svg';
+    })
 });
 
