e4a9f375cf5b4c4c12016e3637bc6a263fd76e26
Nick Kirby <nrkirb@gmail.com>
2019-06-16 10:32:26 +0100

    Begin working on home page

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,35 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import React from 'react'
+import { css } from 'glamor'
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+import avatar from '../images/nrkirby.jpg'
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
+export default function Header() {
+
+  return <div {...ContainerStyles}>
+    <h1>Nick Kirby</h1>
+    <img
+      src={avatar}
+      {...ImageStyles} />
+    <h2>Software Developer</h2>
+  </div>
 }
 
-Header.defaultProps = {
-  siteTitle: ``,
-}
+const ContainerStyles = css({
+  marginTop: 25,
+  padding: '20px 0',
+  color: '#f39c12',
+  textAlign: 'center',
+  height: '500px'
+})
 
-export default Header
+const ImageStyles = css({
+  borderRadius: '50%',
+  height: '150px',
+  width: 'auto',
+  marginLeft: 'auto',
+  marginRight: 'auto',
+  marginTop: '0',
+  marginBottom: '20px'
+})
+
+
